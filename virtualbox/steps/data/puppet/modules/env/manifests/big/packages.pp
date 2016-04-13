6bba4ca882cac63b20c7cd8b1242e210aa0d529a
Arthur Garnier <arthur.garnier@inria.fr>
2016-04-13 10:10:55 +0200

    [puppet/big] Install the packages libnetcdf-dev, libboost-all-dev and cmake-curses-gui (#6541)

--- virtualbox/steps/data/puppet/modules/env/manifests/big/packages.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/big/packages.pp
@@ -7,7 +7,7 @@ class env::big::packages () {
   $utils = [ 'at', 'bash-completion', 'bc', 'connect-proxy', 'kbd', 'debhelper', 'debootstrap', 'diffstat', 'discover', 'discover-data', 'ftp', 'genders', 'gnuplot', 'hdparm', 'html2text', 'hwloc', 'inotify-tools', 'info', 'iperf', 'iputils-arping', 'iputils-tracepath', 'kanif', 'lsb-release', 'mysql-client', 'nmap', 'numactl', 'pv', 'r-base', 'stress', 'nuttcp', 'db-util', 'clustershell', 'parallel', 'cron', 'postgresql-client', 'tmux' ]
 
   # Dev and languages
-  $general_dev = [ 'libreadline6-dev', 'autoconf', 'flex', 'bison', 'libyaml-0-2', 'cgdb', 'cmake', 'cvs', 'gdb', 'gfortran', 'git-core', 'subversion', 'libatlas-base-dev', 'libdate-calc-perl', 'libc6', 'libjson-perl', 'libjson-xs-perl', 'libssl-dev', 'make', 'patch', 'tcl', 'php5-cli', 'valgrind', 'libtool', 'libnuma-dev', 'libdb-dev', 'libatlas-dev', 'debconf-utils' ]
+  $general_dev = [ 'libreadline6-dev', 'autoconf', 'flex', 'bison', 'libyaml-0-2', 'cgdb', 'cmake', 'cmake-curses-gui', 'cvs', 'gdb', 'gfortran', 'git-core', 'subversion', 'libatlas-base-dev', 'libdate-calc-perl', 'libc6', 'libjson-perl', 'libjson-xs-perl', 'libssl-dev', 'make', 'patch', 'tcl', 'php5-cli', 'valgrind', 'libtool', 'libnuma-dev', 'libdb-dev', 'libatlas-dev','debconf-utils', 'libnetcdf-dev', 'libboost-all-dev' ]
   $perl_dev = [ 'libwww-perl', 'libperl-dev', 'libswitch-perl' ]
   $python_dev = [ 'python-dev', 'python-imaging', 'python-matplotlib-data', 'python-matplotlib-doc', 'python-mysqldb', 'python-numpy', 'python-paramiko', 'python-scipy', 'python-sqlite', 'python-httplib2', 'python-yaml', 'python-psycopg2', 'python-pip']
   $java_dev = [ 'openjdk-7-jdk', 'openjdk-7-jre', 'ant' ]
