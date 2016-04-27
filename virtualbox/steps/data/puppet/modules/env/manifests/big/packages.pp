e27ca93569a962e2aefce42bdb817d1d0efecf5f
Arthur Garnier <arthur.garnier@inria.fr>
2016-04-27 11:33:16 +0200

    [puppet/big] Add python3

--- virtualbox/steps/data/puppet/modules/env/manifests/big/packages.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/big/packages.pp
@@ -9,7 +9,7 @@ class env::big::packages () {
   # Dev and languages
   $general_dev = [ 'libreadline6-dev', 'autoconf', 'flex', 'bison', 'libyaml-0-2', 'cgdb', 'cmake', 'cmake-curses-gui', 'cvs', 'gdb', 'gfortran', 'git-core', 'subversion', 'libatlas-base-dev', 'libdate-calc-perl', 'libc6', 'libjson-perl', 'libjson-xs-perl', 'libssl-dev', 'make', 'patch', 'tcl', 'php5-cli', 'valgrind', 'libtool', 'libnuma-dev', 'libdb-dev', 'libatlas-dev','debconf-utils', 'libnetcdf-dev', 'libboost-all-dev' ]
   $perl_dev = [ 'libwww-perl', 'libperl-dev', 'libswitch-perl' ]
-  $python_dev = [ 'python-dev', 'python-imaging', 'python-matplotlib-data', 'python-matplotlib-doc', 'python-mysqldb', 'python-numpy', 'python-paramiko', 'python-scipy', 'python-sqlite', 'python-httplib2', 'python-yaml', 'python-psycopg2', 'python-pip', 'ruby-net-ssh-multi']
+  $python_dev = [ 'python-dev', 'python-imaging', 'python-matplotlib-data', 'python-matplotlib-doc', 'python-mysqldb', 'python-numpy', 'python-paramiko', 'python-scipy', 'python-sqlite', 'python-httplib2', 'python-yaml', 'python-psycopg2', 'python-pip', 'ruby-net-ssh-multi', 'python3']
   $java_dev = [ 'openjdk-7-jdk', 'openjdk-7-jre', 'ant' ]
   case $operatingsystem {
       'centos':           { $dev = [ $general_dev, $perl_dev, $python_dev, $java_dev, 'gcc', 'ruby-libs', 'ruby-devel', 'ruby-docs', 'ruby-rack', 'ruby-ri', 'ruby-irb', 'ruby-rdoc', 'ruby-mode', 'libwww-perl', 'libperl-dev' ]}
