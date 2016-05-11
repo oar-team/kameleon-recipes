94d78864cdfca51295f955312dee9b7ce5786c58
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2016-05-11 11:46:49 +0200

    Add debian-keyring, which used to be installed in min, but isn't anymore

--- virtualbox/steps/data/puppet/modules/env/manifests/base/packages.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/base/packages.pp
@@ -1,7 +1,7 @@
 class env::base::packages () {
 
   # Removed : findutils, grep, gzip, man-db, sed, tar, wget, diffutils, multiarch-support
-  $utils = [ 'bzip2', 'curl', 'dnsutils', 'dtach', 'host', 'ldap-utils', 'lshw', 'lsof', 'bsd-mailx', 'm4', 'netcat-openbsd', 'rsync', 'screen', 'strace', 'taktuk', 'telnet', 'time', 'xstow', 'sudo', 'less', 'vim', 'nano' ]
+  $utils = [ 'bzip2', 'curl', 'dnsutils', 'dtach', 'host', 'ldap-utils', 'lshw', 'lsof', 'bsd-mailx', 'm4', 'netcat-openbsd', 'rsync', 'screen', 'strace', 'taktuk', 'telnet', 'time', 'xstow', 'sudo', 'less', 'vim', 'nano', 'debian-keyring' ]
   $languages = [ 'perl', 'python', 'ipython', 'ruby' ]
 
   $installed = [ $utils, $languages ]
