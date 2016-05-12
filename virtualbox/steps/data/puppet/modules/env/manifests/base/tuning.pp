9a8546616aeac8e8f1d7e3631a7c5acd850b6e8c
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2016-05-12 16:46:15 +0200

    Drop support for setting max number of open files
    65536 is the default now, according to what can be seen on jessie-x64-min

--- virtualbox/steps/data/puppet/modules/env/manifests/base/tuning.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/base/tuning.pp
@@ -1,31 +1,5 @@
 class env::base::tuning (){
 
-# *** Setting up max open file descriptors
-#
-# See: https://www.grid5000.fr/mediawiki/index.php/Max_open_file_descriptors_tuning
-#
-
-  file {
-    '/etc/security/limits.conf':
-      ensure   => file,
-      owner    => root,
-      group    => root,
-      mode     => '0644',
-      source   => 'puppet:///modules/env/base/tuning/limits.conf';
-  }
-  augeas {
-    'pam_limits_modification':
-      context => "/files/etc/pam.d/common-session",
-      incl    => "/etc/pam.d/common-session",
-      lens    => "Pam.lns",
-      onlyif  => "match *[type='session' and control='required' and module='pam_limits.so'] size < 1",
-      changes => [
-                  "ins 0 after *[type='session'][last()]",
-                  "set 0/type session",
-                  "set 0/control required",
-                  "set 0/module pam_limits.so",
-      ];
-  }
 
 # *** Setting up TCP buffers
 #
