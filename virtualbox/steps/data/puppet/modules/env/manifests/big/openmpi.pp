84cdc98990a67a1aed15e4dd4d2a08bbe0f62cb1
Arthur Garnier <arthur.garnier@inria.fr>
2016-05-31 16:03:56 +0200

    [puppet/big] Missing dependency in openmpi

--- virtualbox/steps/data/puppet/modules/env/manifests/big/openmpi.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/big/openmpi.pp
@@ -3,9 +3,12 @@ class env::big::openmpi () {
   $openmpi_packages = [ 'librdmacm1', 'libgfortran3', 'libnuma1', 'blcr-util', 'libibverbs1-dbg', 'libopenmpi1.6', 'openmpi-common', 'openmpi-bin', 'libopenmpi-dev', 'openmpi-checkpoint' ]
 
   package{
+    'libibverbs-dev':
+      ensure => installed;
     $openmpi_packages:
       ensure     => installed,
       require    => Package['libibverbs-dev']
   }
+
 }
 
