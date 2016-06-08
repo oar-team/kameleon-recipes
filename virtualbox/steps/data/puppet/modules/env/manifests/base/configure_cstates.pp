02db819fd5811b52552af58ee2d2099124d3a270
Jérémie Gaidamour <jeremie.gaidamour@inria.fr>
2016-06-08 09:34:47 +0200

    [base] Enable C-States C1, C1E

---
+++ virtualbox/steps/data/puppet/modules/env/manifests/base/configure_cstates.pp
@@ -0,0 +1,24 @@
+# Limit CPU C-States Power Saving Modes to C0, C1 and C1E.
+
+class env::base::configure_cstates {
+
+  file {
+    "/usr/local/sbin/force_cstates_c0-c1e.rb":
+      mode    => '0744',
+      owner   => root,
+      group   => root,
+      source  => "puppet:///modules/env/base/cstates/force_cstates_c0-c1e.rb";
+    "/etc/systemd/system/cstates.service":
+      mode    => '0644',
+      owner   => root,
+      group   => root,
+      source  => "puppet:///modules/env/base/cstates/cstates.service";
+  }
+
+  service {
+    "cstates":
+      ensure  => running,
+      enable  => true,
+      require => File["/usr/local/sbin/force_cstates_c0-c1e.rb", "/etc/systemd/system/cstates.service"];
+  }
+}
