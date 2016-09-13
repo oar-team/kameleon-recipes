2dd1307689111bd89b867a493c8e952a353bdca7
Pierre Neyron <pierre.neyron@imag.fr>
2016-09-13 16:44:43 +0200

    make build take recipes as parameter + some rework

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ROOT_PROJECT=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
-BUILD_PATH=/scratch/build
-ROOTFS_PATH=/scratch/rootfs
+BUILD_PATH=${TARGET:-$PWD}/build
+ROOTFS_PATH=${TARGET:-$PWD}/rootfs
 
 trap ctrl_c INT
 
@@ -12,18 +12,24 @@ function ctrl_c() {
         exit 1
 }
 
-
-for RECIPE_PATH in `ls from_scratch/*.yaml`; do
+if [ -z "$1" ]; then
+	RECIPES=from_scratch/*.yaml
+else
+  RECIPES="$@"
+fi
+for RECIPE_PATH in $RECIPES; do
     RECIPE_NAME=$(basename $RECIPE_PATH .yaml)
-    if [ ! -e "$ROOTFS_PATH/${RECIPE_NAME}.tar.xz" ]; then
+    if [ -e "$ROOTFS_PATH/${RECIPE_NAME}.tar" -a -z "$FORCE" ]; then
+      echo "$ROOTFS_PATH/${RECIPE_NAME}.tar already exists"
+    else
         rm -rf $BUILD_PATH
         echo -e "===============================================================\nRECIPE_NAME"
         echo -e "==============================================================="
         (set -x; kameleon build $ROOT_PROJECT/$RECIPE_PATH --build-path \
-                    $BUILD_PATH --script --enable-cache)
+                    $BUILD_PATH --script --enable-cache --cache-archive-compression=none --global=appliance_formats:tar)
         if [ $? -eq 0 ]; then
             mkdir -p $ROOTFS_PATH
-            mv $BUILD_PATH/$RECIPE_NAME/*.tar.{gz,xz} $ROOTFS_PATH/
+            mv $BUILD_PATH/$RECIPE_NAME/*.tar $ROOTFS_PATH/
         else
             echo "\n$RECIPE_NAME FAILED\n"
         fi
