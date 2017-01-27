2c4d855bf1db4c2adde0752014d7c16fe45d7054
David Loup <david.loup@inria.fr>
2017-01-27 16:23:03 +0100

    [big] Fix ganglia bug 7748

--- virtualbox/steps/data/puppet/modules/env/manifests/big/configure_nvidia_gpu/ganglia.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/big/configure_nvidia_gpu/ganglia.pp
@@ -25,6 +25,17 @@ class env::big::configure_nvidia_gpu::ganglia () {
           mode    => '0644',
           source  => "puppet:///modules/env/big/nvidia/modpython-nvidia.conf",
           require => Package['ganglia-monitor-python-nvidia'];
+        '/etc/systemd/system/ganglia-monitor.service':
+          ensure  => file,
+          owner   => root,
+          group   => root,
+          mode    => '0644',
+          source  => "puppet:///modules/env/big/nvidia/ganglia-monitor.service";
+      }
+      exec {
+       'Enable ganglia on startup':
+         command => "systemctl enable ganglia-monitor",
+         path    => ['/bin','/usr/bin','/sbin'];
       }
     }
     default: {
