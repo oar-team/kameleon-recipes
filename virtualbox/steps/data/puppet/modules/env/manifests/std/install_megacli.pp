d88055633e1cae9c4a0ab32b1d9aaaff3f8c7724
Arthur Garnier <arthur.garnier@inria.fr>
2017-02-16 16:38:43 +0100

    [std] Add gpg key to megacli repo

--- virtualbox/steps/data/puppet/modules/env/manifests/std/install_megacli.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/std/install_megacli.pp
@@ -2,7 +2,10 @@ class env::std::install_megacli {
   include apt
 
   apt::source { 'hwraid.le-vert.net':
-    allow_unsigned => true,
+    key      => {
+      'id'      => '6005210E23B3D3B4',
+      'server'  => 'pool.sks-keyservers.net',
+    },
     comment  => 'Repo for megacli package',
     location => 'http://hwraid.le-vert.net/debian',
     release  => 'jessie',
