ef9458ea0db59f5534fcf97a9ff02bad6735a5cf
Pierre Neyron <pierre.neyron@imag.fr>
2017-04-04 18:19:40 +0200

    rework the debootstrap script and its call.

--- steps/data/debootstrap.sh
+++ steps/data/debootstrap.sh
@@ -7,56 +7,50 @@ fail() {
   exit 1
 }
 
-export http_proxy=
-export https_proxy="$http_proxy"
-export ftp_proxy="$http_proxy"
-export rsync_proxy="$http_proxy"
-export all_proxy="$http_proxy"
-export HTTP_PROXY="$http_proxy"
-export HTTPS_PROXY="$http_proxy"
-export FTP_PROXY="$http_proxy"
-export RSYNC_PROXY="$http_proxy"
-export ALL_PROXY="$http_proxy"
-export no_proxy="localhost,$(echo $http_proxy | tr ":" "\n" | head -n 1),127.0.0.1,localaddress,.localdomain"
-export NO_PROXY="$no_proxy"
+[ -n "$KAMELEON_DEBUG" ] && set -x
 
+[ -n "$CHROOT_DIR" ] || fail "No CHROOT_DIR defined"
+[ -n "$RELEASE" ] || fail "No RELEASE defined"
 
-MNT=
-RELEASE=
-REPOSITORY="http://http.debian.net/debian/"
-HOSTNAME="localhost"
-LOCALES="POSIX C en_US fr_FR de_DE"
-LANG="en_US.UTF-8"
+REPOSITORY=${REPOSITORY:-http://http.debian.net/debian/}
+HOSTNAME=${HOSTNAME:-localhost}
+LOCALES=${LOCALES:-POSIX C en_US}
+LANG=${LANG:-en_US.UTF-8}
 CURRENT_ARCH="$(uname -m)"
-ARCH="$CURRENT_ARCH"
-DEBIAN_ARCH="$(dpkg --print-architecture)"
-DEBIAN_KERNEL_ARCH="$DEBIAN_ARCH"
-PACKAGES="less,locales,vim,openssh-server,linux-image-$DEBIAN_KERNEL_ARCH"
-VARIANT=
+ARCH=${ARCH:-$CURRENT_ARCH}
+DEBIAN_ARCH=${DEBIAN_ARCH:-$(dpkg --print-architecture)}
+DEBIAN_KERNEL_ARCH=${DEBIAN_KERNEL_ARCH:-$DEBIAN_ARCH}
 
-if [ -n "$VARIANT" ]; then
-  VARIANT_OPT="--variant=$VARIANT"
-fi
-
-if [ -n "$PACKAGES" ]; then
-  PACKAGES_OPT="--include=\"$PACKAGES\""
+if [ -n "$PROXY" ]; then
+  export http_proxy="http://$PROXY"
+  export https_proxy="$http_proxy"
+  export ftp_proxy="$http_proxy"
+  export rsync_proxy="$http_proxy"
+  export all_proxy="$http_proxy"
+  export HTTP_PROXY="$http_proxy"
+  export HTTPS_PROXY="$http_proxy"
+  export FTP_PROXY="$http_proxy"
+  export RSYNC_PROXY="$http_proxy"
+  export ALL_PROXY="$http_proxy"
+  export no_proxy="localhost,${PROXY%%:*},127.0.0.1,localaddress,.localdomain"
+  export NO_PROXY="$no_proxy"
 fi
 
 if [ $CURRENT_ARCH == $ARCH ]; then
-  debootstrap --arch=$DEBIAN_ARCH --no-check-gpg $VARIANT_OPT $PACKAGES_OPT $RELEASE $MNT $REPOSITORY
-  CHROOT_CMD="chroot $MNT"
+  debootstrap --arch=$DEBIAN_ARCH --no-check-gpg ${VARIANT:+--variant=$VARIANT} ${PACKAGES:+--include="$PACKAGES"} $RELEASE $CHROOT_DIR $REPOSITORY
+  CHROOT_CMD="chroot $CHROOT_DIR"
 else
-  debootstrap --foreign --arch=$DEBIAN_ARCH --no-check-gpg $VARIANT_OPT $PACKAGES_OPT $RELEASE $MNT $REPOSITORY
-  cp /usr/bin/qemu-$ARCH-static $MNT/usr/bin/
-  CHROOT_CMD="chroot $MNT /usr/bin/qemu-$ARCH-static /bin/bash"
+  debootstrap --foreign --arch=$DEBIAN_ARCH --no-check-gpg ${VARIANT:+--variant=$VARIANT} ${PACKAGES:+--include="$PACKAGES"} $RELEASE $CHROOT_DIR $REPOSITORY
+  cp /usr/bin/qemu-$ARCH-static $CHROOT_DIR/usr/bin/
+  CHROOT_CMD="chroot $CHROOT_DIR /usr/bin/qemu-$ARCH-static /bin/bash"
   $CHROOT_CMD /debootstrap/debootstrap --second-stage
 fi
 
 #Setting Hostname, network and lang
-echo $HOSTNAME >> $MNT/etc/hostname
+echo $HOSTNAME >> $CHROOT_DIR/etc/hostname
 
 # Configure network interfaces
-cat > $MNT/etc/network/interfaces <<EOF
+cat > $CHROOT_DIR/etc/network/interfaces <<EOF
 auto lo
 iface lo inet loopback
 
@@ -64,15 +58,15 @@ auto eth0
 iface eth0 inet dhcp
 EOF
 
-echo LANG=$LANG > $MNT/etc/locale.conf
+echo LANG=$LANG > $CHROOT_DIR/etc/locale.conf
 
-test -f $MNT/etc/mtab || cat /proc/mounts > $MNT/etc/mtab
-cat /etc/resolv.conf > $MNT/etc/resolv.conf
+test -f $CHROOT_DIR/etc/mtab || cat /proc/mounts > $CHROOT_DIR/etc/mtab
+cat /etc/resolv.conf > $CHROOT_DIR/etc/resolv.conf
 
 # Configure locales
-if [ -f $MNT/etc/locale.gen ]; then
+if [ -f $CHROOT_DIR/etc/locale.gen ]; then
   for l in $LOCALES; do
-    sed -i -e "s/^#$l/$l/" $MNT/etc/locale.gen
+    sed -i -e "s/^#$l/$l/" $CHROOT_DIR/etc/locale.gen
   done
   $CHROOT_CMD /usr/sbin/locale-gen
 fi
