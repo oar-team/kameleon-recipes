36fce11743c2eb67e3cc575609abb9cb99dc659a
Florent Didier <florent.didier@inria.fr>
2017-04-12 09:52:24 +0200

    [std] Use g5k-checks version 0.7.12

--- virtualbox/steps/data/puppet/modules/env/manifests/std/install_g5kchecks.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/std/install_g5kchecks.pp
@@ -3,7 +3,7 @@ class env::std::install_g5kchecks {
   include 'env::std::ipmitool' # ipmitool is required by g5k-checks
   include 'env::std::dell'     # dell tools are required by g5k-checks
 
-  $g5kchecks_version = "0.7.11"
+  $g5kchecks_version = "0.7.12"
 
   $g5kchecks_deps = [ 'ruby-rest-client', 'ohai', 'ruby-popen4', 'fio', 'ruby-json', 'x86info' ]
   case $operatingsystem {
