fe7607b7321be7667b83a132bf7604bed1014c42
Nicolas Michon <nicolas.michon@inria.fr>
2017-08-30 18:18:35 +0200

    [std] Updated g5k-checks version to 0.7.13 and changed package target arch to _amd64 instead of _all

--- virtualbox/steps/data/puppet/modules/env/manifests/std/install_g5kchecks.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/std/install_g5kchecks.pp
@@ -3,7 +3,7 @@ class env::std::install_g5kchecks {
   include 'env::std::ipmitool' # ipmitool is required by g5k-checks
   include 'env::std::dell'     # dell tools are required by g5k-checks
 
-  $g5kchecks_version = "0.7.12"
+  $g5kchecks_version = "0.7.13"
 
   $g5kchecks_deps = [ 'ruby-rest-client', 'ohai', 'ruby-popen4', 'fio', 'ruby-json', 'x86info' ]
   case $operatingsystem {
@@ -12,14 +12,14 @@ class env::std::install_g5kchecks {
       require env::commonpackages::rubyrspec
       exec {
         "retrieve_g5kchecks":
-          command  => "/usr/bin/wget --no-check-certificate -q https://www.grid5000.fr/packages/debian/g5kchecks_${g5kchecks_version}_all.deb -O /tmp/g5kchecks_${g5kchecks_version}_all.deb",
-          creates  => "/tmp/g5kchecks_${g5kchecks_version}_all.deb";
+          command  => "/usr/bin/wget --no-check-certificate -q https://www.grid5000.fr/packages/debian/g5kchecks_${g5kchecks_version}_amd64.deb -O /tmp/g5kchecks_${g5kchecks_version}_amd64.deb",
+          creates  => "/tmp/g5kchecks_${g5kchecks_version}_amd64.deb";
       }
       package {
         "g5kchecks":
           ensure   => installed,
           provider => dpkg,
-          source   => "/tmp/g5kchecks_${g5kchecks_version}_all.deb",
+          source   => "/tmp/g5kchecks_${g5kchecks_version}_amd64.deb",
           require  => [ Exec["retrieve_g5kchecks"], Package[$g5kchecks_deps], Package['rake'], Package['ntp'] ];
         $g5kchecks_deps:
           ensure   => installed;
