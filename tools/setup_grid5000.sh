c30c3fe37af151297aab52d02f40beb2b974b905
Olivier Demengeon <olivier.demengeon@inria.fr>
2018-01-10 11:18:54 +0100

    Improve tools installation to build image on debian9 distribution

--- tools/setup_grid5000.sh
+++ tools/setup_grid5000.sh
@@ -2,7 +2,7 @@
 set -e
 echo "libguestfs0     libguestfs/update-appliance     boolean false" | debconf-set-selections
 echo "mdadm   mdadm/initrdstart       string  none" | debconf-set-selections
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends git virtualbox linux-headers-amd64 socat qemu-utils ruby-dev ruby-childprocess polipo pigz
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends git virtualbox linux-headers-amd64 socat qemu-utils ruby-dev ruby-childprocess polipo pigz linux-headers-$(uname -r) netcat 
 apt-get install -y libguestfs-tools
 apt-get install -y dirmngr 
 gem install --no-ri --no-rdoc kameleon-builder
