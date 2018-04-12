308f2bbfd1ecb49465572846dcd8e76ce2f59aa9
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-04-12 21:59:11 +0200

    Synchronize both setup_grid5000 scripts (and drop tmpfs usage from both for consistency)

--- tools/setup_grid5000.sh
+++ tools/setup_grid5000.sh
@@ -2,14 +2,10 @@
 set -e
 echo "libguestfs0     libguestfs/update-appliance     boolean false" | debconf-set-selections
 echo "mdadm   mdadm/initrdstart       string  none" | debconf-set-selections
-apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends git virtualbox linux-headers-amd64 socat qemu-utils ruby-dev ruby-childprocess polipo pigz linux-headers-$(uname -r) netcat 
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends git virtualbox linux-headers-amd64 socat qemu-utils ruby-dev ruby-childprocess polipo pigz linux-headers-$(uname -r) netcat
 apt-get install -y libguestfs-tools
-apt-get install -y dirmngr 
+apt-get install -y dirmngr
 gem install --no-ri --no-rdoc kameleon-builder
-memSize=`grep MemTotal /proc/meminfo | tr -s ' ' | cut -f 2 -d ' '`
-if [ "$memSize" -gt "27262976" ]; then
-  #mount -t tmpfs tmpfs /tmp
-  mv /bin/gzip /bin/gzip.OLD
-  ln -s /usr/bin/pigz /bin/gzip
-fi
+mv /bin/gzip /bin/gzip.OLD
+ln -s /usr/bin/pigz /bin/gzip
 cd /tmp
