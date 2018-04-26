4957d5b7e404597d2096ea4038ef43f9560e5b3d
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-04-26 21:51:58 +0200

    Add a systemtap script that disables fsyncs

--- tools/setup_grid5000.sh
+++ tools/setup_grid5000.sh
@@ -5,6 +5,11 @@ echo "mdadm   mdadm/initrdstart       string  none" | debconf-set-selections
 apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends git virtualbox linux-headers-amd64 socat qemu-utils ruby-dev ruby-childprocess polipo pigz linux-headers-$(uname -r) netcat eatmydata
 apt-get install -y libguestfs-tools
 apt-get install -y dirmngr
+
+# run a systemtap script that disables fsyncs
+apt-get install -y systemtap linux-image-$(uname -r)-dbg
+/tmp/environments-recipes/tools/nofsync.stp </dev/null >/dev/null 2>&1 &
+
 gem install --no-ri --no-rdoc kameleon-builder
 mv /bin/gzip /bin/gzip.OLD
 ln -s /usr/bin/pigz /bin/gzip
