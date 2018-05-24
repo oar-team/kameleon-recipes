950ab90475c827e03cffa24bea3d02e93d2c72ed
David Loup <david.loup@inria.fr>
2018-05-24 09:28:55 +0200

    [debian9,jessie] Fetch hwraid gpg key from file instead of online (bug 9234)

--- virtualbox/steps/data/puppet/modules/env/manifests/std/install_hwraid_apt_source.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/std/install_hwraid_apt_source.pp
@@ -5,7 +5,7 @@ class env::std::install_hwraid_apt_source {
   apt::source { 'hwraid.le-vert.net':
     key      => {
       'id'      => '6005210E23B3D3B4',
-      'server'  => 'pool.sks-keyservers.net',
+      'content' => template('env/std/hwraid/hwraid.le-vert.net.key.erb'),
     },
     comment  => 'Repo for megacli package',
     location => 'http://hwraid.le-vert.net/debian',
@@ -16,6 +16,4 @@ class env::std::install_hwraid_apt_source {
       'src' => false
     }
    }
-
-
 }
