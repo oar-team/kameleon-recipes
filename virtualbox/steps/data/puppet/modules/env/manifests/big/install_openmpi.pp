7b4edbce2bcbc3ff1dd773015cc01575ac8a7717
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-06-18 14:04:22 +0200

    Add psm2-dev to installation of openmpi, and install from Scibian

--- virtualbox/steps/data/puppet/modules/env/manifests/big/install_openmpi.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/big/install_openmpi.pp
@@ -1,18 +1,32 @@
 class env::big::install_openmpi () {
 
   if "${::lsbdistcodename}" == "stretch" {
-    $openmpi_packages = [ 'librdmacm1', 'libgfortran3', 'libnuma1', 'blcr-util', 'libibverbs1-dbg', 'libopenmpi2', 'openmpi-common', 'openmpi-bin', 'libopenmpi-dev' ]
+    $openmpi_deps_packages = [ 'librdmacm1', 'libgfortran3', 'libnuma1', 'blcr-util', 'libibverbs1-dbg', 'libibverbs-dev', 'libpsm2-dev', 'libhfi1-dev', 'libopamgt-dev' ]
+    $openmpi_packages = [ 'libopenmpi-dev', 'openmpi-bin' ]
+    $openmpi_scibian_version = '2.0.2-2sci9+opa10.6u1'
+
+    package{
+      $openmpi_deps_packages:
+        ensure     => installed,
+        require  => [Apt::Source['scibian9opa10.6'], Exec['apt_update']]
+    }
+
+    package {
+      $openmpi_packages:
+        ensure     => $openmpi_scibian_version,
+        require  => [Apt::Source['scibian9opa10.6'], Exec['apt_update']]
+    }
+
   } else {
     $openmpi_packages = [ 'librdmacm1', 'libgfortran3', 'libnuma1', 'blcr-util', 'libibverbs1-dbg', 'libopenmpi1.6', 'openmpi-common', 'openmpi-bin', 'libopenmpi-dev', 'openmpi-checkpoint' ]
-  }
 
-  package{
-    'libibverbs-dev':
-      ensure => installed;
-    $openmpi_packages:
-      ensure     => installed,
-      require    => Package['libibverbs-dev']
+    package{
+      'libibverbs-dev':
+        ensure => installed;
+      $openmpi_packages:
+        ensure     => installed,
+        require    => Package['libibverbs-dev']
+    }
   }
-
 }
 
