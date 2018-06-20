a21b72211b55e390a70919d40145dabf79c589e0
acourbet <adrien.courbet@inria.fr>
2018-06-20 15:56:10 +0200

    cuda : remove  from /etc/profile

--- virtualbox/steps/data/puppet/modules/env/manifests/big/configure_nvidia_gpu/cuda.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/big/configure_nvidia_gpu/cuda.pp
@@ -72,9 +72,6 @@ file_line {
   'cuda_etc_profile_path':
      path => '/etc/profile',
      line => 'export PATH=$PATH:/usr/local/cuda/bin';
-  'ld_library_path':
-     path => '/etc/profile',
-     line => 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64';
   }
 
   package{
