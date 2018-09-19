d778eca26a14a362c21f13d5629aa97e95a89a2e
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-09-19 13:30:05 +0200

    Fetch sudo-g5k from packages.grid5000.fr

--- virtualbox/steps/data/puppet/modules/env/manifests/std/install_sudog5k.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/std/install_sudog5k.pp
@@ -5,23 +5,30 @@ class env::std::install_sudog5k {
       require env::commonpackages::rake
       require env::commonpackages::rubyrspec
       require env::commonpackages::rsyslog
-      exec {
-        "retrieve_sudog5k":
-          command  => "/usr/bin/wget --no-check-certificate -q https://www.grid5000.fr/packages/debian/sudo-g5k_all.deb -O /tmp/sudo-g5k_all.deb",
-          creates  => "/tmp/sudo-g5k_all.deb";
+
+      apt::source { 'sudo-g5k':
+        key      => {
+          'id'      => '3C38BDEAA05D4A7BED7815E5B1F34F56797BF2D1',
+          'content' => file('env/min/apt/grid5000-archive-key.asc')
+        },
+        comment  => 'Grid5000 repository for sudo-g5k',
+        location => 'http://packages.grid5000.fr/deb/sudo-g5k/',
+        release  => "/",
+        repos    => '',
+        include  => { 'deb' => true, 'src' => false }
       }
-      package {
-        "sudo-g5k":
-          ensure   => installed,
-          provider => dpkg,
-          source   => "/tmp/sudo-g5k_all.deb",
-          require  => [Exec["retrieve_sudog5k"]];
+
+
+      package { 'sudo-g5k':
+        ensure  => '1.1',
+        require => Class['apt::update']
       }
     }
     default: {
       err "${operatingsystem} not suported."
     }
   }
+
   file {
     '/etc/sudo-g5k/id_rsa_sudo-g5k':
       ensure  => file,
