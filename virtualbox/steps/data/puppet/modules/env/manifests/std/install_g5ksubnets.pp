5b6cf327878ce8a968899a45c23e8a77500d5661
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2018-09-20 16:49:19 +0200

    [g5k-subnets] moved to packages.grid5000.fr

--- virtualbox/steps/data/puppet/modules/env/manifests/std/install_g5ksubnets.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/std/install_g5ksubnets.pp
@@ -1,15 +1,20 @@
 class env::std::install_g5ksubnets {
 
-  exec {
-    "retrieve_g5ksubnets":
-      command => "/usr/bin/wget --no-check-certificate -q https://www.grid5000.fr/packages/debian/g5k-subnets.gem -O /tmp/g5k-subnets.gem",
-      creates => "/tmp/g5k-subnets.gem";
+  apt::source { 'g5k-subnets':
+    key      => {
+      'id'      => '3C38BDEAA05D4A7BED7815E5B1F34F56797BF2D1',
+      'content' => file('env/min/apt/grid5000-archive-key.asc')
+    },
+    comment  => 'Grid5000 repository for g5k-subnets',
+    location => 'http://packages.grid5000.fr/deb/g5k-subnets/',
+    release  => "/",
+    repos    => '',
+    include  => { 'deb' => true, 'src' => false }
   }
+
   package {
-    "g5k-subnets":
-      ensure  => installed,
-      provider => gem,
-      source   => "/tmp/g5k-subnets.gem",
-      require  =>  Exec["retrieve_g5ksubnets"];
+    'g5k-subnets':
+      ensure   => '1.4',
+      require  =>  Class['apt::update'];
   }
 }
