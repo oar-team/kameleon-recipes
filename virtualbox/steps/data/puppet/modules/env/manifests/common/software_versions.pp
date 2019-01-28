22688ac5a65708694247dec3258533cafc638701
Pascal Morillon <pascal.morillon@irisa.fr>
2019-01-28 10:22:16 +0100

    [env::common::software_versions] Upgrade g5k-mata-packages to 0.7.21

--- virtualbox/steps/data/puppet/modules/env/manifests/common/software_versions.pp
+++ virtualbox/steps/data/puppet/modules/env/manifests/common/software_versions.pp
@@ -2,7 +2,7 @@
 
 class env::common::software_versions {
    $g5k_subnets = '1.4.2'
-   $g5k_meta_packages = '0.7.20'
+   $g5k_meta_packages = '0.7.21'
    $tgz_g5k = '1.0.12'
    $g5k_checks = '0.8.6'
    $sudo_g5k = '1.1'
