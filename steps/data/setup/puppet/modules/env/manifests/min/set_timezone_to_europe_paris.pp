e241928184f490e33aaedf12cda538f65de0b538
Teddy Valette <teddy.valette@inria.fr>
2019-04-11 17:22:41 +0200

    [env::min::set_timezone_to_europe_paris] fix timezone to Europe/Paris

--- steps/data/setup/puppet/modules/env/manifests/min/set_timezone_to_europe_paris.pp
+++ steps/data/setup/puppet/modules/env/manifests/min/set_timezone_to_europe_paris.pp
@@ -3,8 +3,7 @@ class env::min::set_timezone_to_europe_paris {
   # Set timezone
   file {
     '/etc/localtime':
-      ensure   => present,
-      source   => '/usr/share/zoneinfo/Europe/Paris';
+      ensure => link,
+      target => '/usr/share/zoneinfo/Europe/Paris',
   }
-
 }
