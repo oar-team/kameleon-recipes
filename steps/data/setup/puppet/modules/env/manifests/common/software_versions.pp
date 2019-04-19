84b199ebd80bc921e22479f02fd972e05c7c21eb
Teddy Valette <teddy.valette@inria.fr>
2019-04-19 15:06:30 +0200

    [env::common::software_versions] Upgrade g5k_meta_packages to 0.7.23

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -2,7 +2,7 @@
 
 class env::common::software_versions {
    $g5k_subnets = '1.4.2'
-   $g5k_meta_packages = '0.7.22'
+   $g5k_meta_packages = '0.7.23'
    $tgz_g5k = '1.0.12'
    $g5k_checks = '0.8.7'
    $sudo_g5k = '1.4'
