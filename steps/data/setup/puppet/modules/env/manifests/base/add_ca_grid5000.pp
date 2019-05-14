efebb765f5ee57c13c6ec186cd50f305db06f567
David Loup <david.loup@inria.fr>
2019-05-14 16:39:20 +0200

    [env::base::add_ca_grid5000] Add ca2019.grid5000.fr.crt as trusted certificate

--- steps/data/setup/puppet/modules/env/manifests/base/add_ca_grid5000.pp
+++ steps/data/setup/puppet/modules/env/manifests/base/add_ca_grid5000.pp
@@ -3,12 +3,15 @@
 class env::base::add_ca_grid5000 {
 
   exec {
-    'get_cert':
+    'get_old_cert':
       command => "/usr/bin/wget --no-check-certificate -q https://www.grid5000.fr/certs/ca.grid5000.fr.crt -O /usr/local/share/ca-certificates/ca.grid5000.fr.crt",
       creates => "/usr/local/share/ca-certificates/ca.grid5000.fr.crt";
+    'get_2019_cert':
+      command => "/usr/bin/wget --no-check-certificate -q https://www.grid5000.fr/certs/ca2019.grid5000.fr.crt -O /usr/local/share/ca-certificates/ca2019.grid5000.fr.crt",
+      creates => "/usr/local/share/ca-certificates/ca2019.grid5000.fr.crt";
     'update_ca':
       command => "/usr/sbin/update-ca-certificates",
-      require => Exec['get_cert'];
+      require => Exec['get_old_cert','get_2019_cert'];
     }
 
 }
