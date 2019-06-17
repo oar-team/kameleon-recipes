d341a1821a6d210867dedeb94c55fd22645c1b24
Teddy Valette <teddy.valette@inria.fr>
2019-06-17 11:47:17 +0200

    [env::base::configure_ip_over_infiniband] use opensm instead of openib for debian10

--- steps/data/setup/puppet/modules/env/manifests/base/configure_ip_over_infiniband.pp
+++ steps/data/setup/puppet/modules/env/manifests/base/configure_ip_over_infiniband.pp
@@ -1,65 +1,70 @@
 class env::base::configure_ip_over_infiniband (){
 
-  #  old packages [ 'ibverbs-utils', 'libibverbs-dev', 'libcxgb3-dev', 'libipathverbs-dev', 'libmlx4-dev', 'libmthca-dev', 'librdmacm-dev', 'rdmacm-utils', 'ibutils', 'infiniband-diags', 'perftest', 'qlvnictools', 'srptools' ]
-  $infiniband_packages = ['qlvnictools']
-  $installed = [ $infiniband_packages, 'syslinux' ]
+  case "${::lsbdistcodename}" {
+    'buster': {
 
-  package {
-    $installed:
-      ensure  => installed;
-  }
+      $infiniband_packages = ['opensm']
+      ensure_packages([$infiniband_packages], {'ensure' => 'installed'})
 
-  file {
-    '/etc/infiniband/openib.conf':
-      ensure   => file,
-      owner    => root,
-      group    => root,
-      mode     => '0644',
-      source   => 'puppet:///modules/env/base/infiniband/openib.conf',
-      require  => Package[$infiniband_packages];
-    '/etc/init.d/openibd':
-      ensure   => file,
-      owner    => root,
-      group    => root,
-      mode     => '0755',
-      source   => 'puppet:///modules/env/base/infiniband/openibd',
-      require  => Package[$infiniband_packages];
-    '/etc/systemd/system/openibd.service':
-      ensure   => file,
-      owner    => root,
-      group    => root,
-      mode     => '0644',
-      source   => 'puppet:///modules/env/base/infiniband/openibd.service',
-      require  => Package[$infiniband_packages];
-   '/lib/udev/rules.d/90-ib.rules':
-      ensure  => present,
-      owner   => root,
-      group   => root,
-      mode    => '0644',
-      source  => 'puppet:///modules/env/base/infiniband/90-ib.rules';
-  }
-
-  if "${::lsbdistcodename}" == "stretch" {
-    service {
-      'openibd':
-        provider => 'systemd',
-        enable   => true,
-        require  => [
-                 Package[$infiniband_packages],
-                 File['/etc/systemd/system/openibd.service']
-      ];
     }
-  } else {
-    if "${::lsbdistcodename}" == "jessie" {
-      service {
-        'openibd':
-          enable   => true,
-          require  => [
-                   Package[$infiniband_packages],
-                   File['/etc/init.d/openibd']
-        ];
+
+    default: {
+      $infiniband_packages = ['qlvnictools', 'syslinux']
+
+      ensure_packages([$infiniband_packages], {'ensure' => 'installed'})
+
+      file {
+        '/etc/infiniband/openib.conf':
+          ensure   => file,
+          owner    => root,
+          group    => root,
+          mode     => '0644',
+          source   => 'puppet:///modules/env/base/infiniband/openib.conf',
+          require  => Package[$infiniband_packages];
+        '/etc/init.d/openibd':
+          ensure   => file,
+          owner    => root,
+          group    => root,
+          mode     => '0755',
+          source   => 'puppet:///modules/env/base/infiniband/openibd',
+          require  => Package[$infiniband_packages];
+        '/etc/systemd/system/openibd.service':
+          ensure   => file,
+          owner    => root,
+          group    => root,
+          mode     => '0644',
+          source   => 'puppet:///modules/env/base/infiniband/openibd.service',
+          require  => Package[$infiniband_packages];
+        '/lib/udev/rules.d/90-ib.rules':
+          ensure  => present,
+          owner   => root,
+          group   => root,
+          mode    => '0644',
+          source  => 'puppet:///modules/env/base/infiniband/90-ib.rules';
+      }
+
+      if "${::lsbdistcodename}" == "stretch" {
+        service {
+          'openibd':
+            provider => 'systemd',
+            enable   => true,
+            require  => [
+              Package[$infiniband_packages],
+              File['/etc/systemd/system/openibd.service']
+            ];
+        }
+      } else {
+        if "${::lsbdistcodename}" == "jessie" {
+          service {
+            'openibd':
+              enable   => true,
+              require  => [
+                Package[$infiniband_packages],
+                File['/etc/init.d/openibd']
+              ];
+          }
+        }
       }
     }
   }
-
 }
