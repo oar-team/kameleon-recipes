0e0fef2d3275e49ddef873817e8d95ca77671cf0
Teddy Valette <teddy.valette@inria.fr>
2019-07-18 15:40:30 +0200

    [big] remove xeon phi recipes

--- steps/data/setup/puppet/modules/env/manifests/big.pp
+++ steps/data/setup/puppet/modules/env/manifests/big.pp
@@ -19,13 +19,6 @@ class env::big ( $variant = "big", $parent_parameters = {} ){
   class { 'env::big::configure_kvm': }
   # nvidia
   class { 'env::big::configure_nvidia_gpu': }
-  if "${::lsbdistcodename}" == "jessie" {
-    # xeon phi, only on jessie. currently no package compiled for stretch
-    class {
-      'env::big::install_xeon_phi_requirements':
-        enable  => $parameters['mic_enable']
-    }
-  }
   #Allow sshfs
   class { 'env::big::configure_sshfs': }
   # Config OpenMPI
