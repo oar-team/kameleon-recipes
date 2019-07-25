3d0ddeb45692769c584d3a746890e9aef0089344
Teddy Valette <teddy.valette@inria.fr>
2019-07-25 10:13:31 +0200

    [env::big::install_beegfs] upgrade to 7.1 with buster

--- steps/data/setup/puppet/modules/env/manifests/big/install_beegfs.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/install_beegfs.pp
@@ -1,7 +1,7 @@
 class env::big::install_beegfs {
 
   case "${::lsbdistcodename}" {
-    "stretch", "buster" : {
+    "stretch" : {
 
       apt::source { 'beegfs':
           location     => 'https://www.beegfs.com/release/beegfs_7/',
@@ -15,7 +15,7 @@ class env::big::install_beegfs {
       }
       -> package { # client
           [ 'beegfs-utils', 'beegfs-helperd', 'beegfs-client', 'linux-headers-amd64', 'beegfs-opentk-lib' ]:
-          require => Class['apt::update'], 
+          require => Class['apt::update'],
           ensure => installed;
       }
       -> service { [ 'beegfs-helperd', 'beegfs-client'] :
@@ -24,6 +24,39 @@ class env::big::install_beegfs {
       }
       -> exec { "beegfs-setup-rdma":
         command => "/usr/sbin/beegfs-setup-rdma -i on"
+      }
+
+      file { '/etc/beegfs/beegfs-client-autobuild.conf':
+          content => "buildEnabled=true\nbuildArgs=-j8 BEEGFS_OPENTK_IBVERBS=1\n",
+          require => Package['beegfs-client']
+      }
+      -> exec {
+      '/etc/init.d/beegfs-client rebuild':
+          timeout => 1200,
+          refreshonly => true
+      }
+    }
+
+    "buster" : {
+
+      apt::source { 'beegfs':
+          location     => 'https://www.beegfs.com/release/beegfs_7_1/',
+          release      => 'deb9', #FIXME : change release to buster when beegfs release it
+          repos        => 'non-free',
+          architecture => 'amd64',
+          key          => {
+              id       => '055D000F1A9A092763B1F0DD14E8E08064497785',
+              source   => 'https://www.beegfs.io/release/beegfs_7/gpg/DEB-GPG-KEY-beegfs',
+          },
+      }
+      -> package { # client
+          [ 'beegfs-utils', 'beegfs-helperd', 'beegfs-client', 'linux-headers-amd64', 'libbeegfs-ib' ]:
+          require => Class['apt::update'],
+          ensure => installed;
+      }
+      -> service { [ 'beegfs-helperd', 'beegfs-client'] :
+        provider => systemd,
+        enable => false,
       }
 
       file { '/etc/beegfs/beegfs-client-autobuild.conf':
