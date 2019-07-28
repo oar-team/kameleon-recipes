194075a47e26ad569421198011b262825f3b8eec
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2019-07-28 07:50:06 +0200

    Upgrade g5k-checks to 0.8.10

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -4,6 +4,6 @@ class env::common::software_versions {
    $g5k_subnets = '1.4.2'
    $g5k_meta_packages = '0.7.24'
    $tgz_g5k = '1.0.12'
-   $g5k_checks = '0.8.9'
+   $g5k_checks = '0.8.10'
    $sudo_g5k = '1.4'
 }
