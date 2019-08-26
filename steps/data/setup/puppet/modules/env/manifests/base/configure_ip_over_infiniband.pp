a4572f50a3c0fb5ed119fa2ff98eb20c76baaee5
Teddy Valette <teddy.valette@inria.fr>
2019-08-26 16:17:45 +0200

    [env::base::configure_ip_over_infiniband] configure openib for debian10

--- steps/data/setup/puppet/modules/env/manifests/base/configure_ip_over_infiniband.pp
+++ steps/data/setup/puppet/modules/env/manifests/base/configure_ip_over_infiniband.pp
@@ -2,46 +2,23 @@ class env::base::configure_ip_over_infiniband (){
 
   case "${::lsbdistcodename}" {
     'buster': {
-
       # En suivant la doc https://wiki.debian.org/RDMA, vous n'avez pas besoin d'installer opensm sur les environnements
       # Il risque de rentrer en conflit avec d'autres instances d'OpenSM présent sur du matériel réseau, ou bien sur des clusters externes à Grid5000 (exemple : https://intranet.grid5000.fr/bugzilla/show_bug.cgi?id=10747)
 
+      service {
+        'openibd':
+          provider => 'systemd',
+          enable   => true,
+          require  => [
+            File['/etc/systemd/system/openibd.service']
+          ];
+      }
     }
 
     default: {
-      $infiniband_packages = ['qlvnictools', 'syslinux']
+      $infiniband_packages = ['qlvnictools']
 
       ensure_packages([$infiniband_packages], {'ensure' => 'installed'})
-
-      file {
-        '/etc/infiniband/openib.conf':
-          ensure   => file,
-          owner    => root,
-          group    => root,
-          mode     => '0644',
-          source   => 'puppet:///modules/env/base/infiniband/openib.conf',
-          require  => Package[$infiniband_packages];
-        '/etc/init.d/openibd':
-          ensure   => file,
-          owner    => root,
-          group    => root,
-          mode     => '0755',
-          source   => 'puppet:///modules/env/base/infiniband/openibd',
-          require  => Package[$infiniband_packages];
-        '/etc/systemd/system/openibd.service':
-          ensure   => file,
-          owner    => root,
-          group    => root,
-          mode     => '0644',
-          source   => 'puppet:///modules/env/base/infiniband/openibd.service',
-          require  => Package[$infiniband_packages];
-        '/lib/udev/rules.d/90-ib.rules':
-          ensure  => present,
-          owner   => root,
-          group   => root,
-          mode    => '0644',
-          source  => 'puppet:///modules/env/base/infiniband/90-ib.rules';
-      }
 
       if "${::lsbdistcodename}" == "stretch" {
         service {
@@ -66,5 +43,38 @@ class env::base::configure_ip_over_infiniband (){
         }
       }
     }
+  }
+
+  file {
+    '/etc/infiniband':
+      ensure   => directory,
+      owner    => root,
+      group    => root,
+      mode     => '0644';
+    '/etc/infiniband/openib.conf':
+      ensure   => file,
+      owner    => root,
+      group    => root,
+      mode     => '0644',
+      source   => 'puppet:///modules/env/base/infiniband/openib.conf',
+      require  => File['/etc/infiniband'];
+    '/etc/init.d/openibd':
+      ensure   => file,
+      owner    => root,
+      group    => root,
+      mode     => '0755',
+      source   => 'puppet:///modules/env/base/infiniband/openibd';
+    '/etc/systemd/system/openibd.service':
+      ensure   => file,
+      owner    => root,
+      group    => root,
+      mode     => '0644',
+      source   => 'puppet:///modules/env/base/infiniband/openibd.service';
+    '/lib/udev/rules.d/90-ib.rules':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      source  => 'puppet:///modules/env/base/infiniband/90-ib.rules';
   }
 }
