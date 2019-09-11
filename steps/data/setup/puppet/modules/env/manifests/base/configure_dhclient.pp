64afaa1467f7808719cf0e36ab1162de2d2c2cf5
Lucas Nussbaum <lucas.nussbaum@loria.fr>
2019-09-11 20:57:54 +0200

    [dhclient] set timeout to 90 (default is 60)

--- steps/data/setup/puppet/modules/env/manifests/base/configure_dhclient.pp
+++ steps/data/setup/puppet/modules/env/manifests/base/configure_dhclient.pp
@@ -3,7 +3,14 @@ class env::base::configure_dhclient () {
   file_line { 'dhclient_interval':
     ensure => present,
     path   => '/etc/dhcp/dhclient.conf',
-    line   => 'initial-interval 1;',
+    line   => 'initial-interval 1; # retry more frequently in case packets get lost',
     match  => '.*initial-interval.*',
+  }
+
+  file_line { 'dhclient_timeout':
+    ensure => present,
+    path   => '/etc/dhcp/dhclient.conf',
+    line   => 'timeout 90; # slow clusters can take more than 60s (bug #10716, grisou)',
+    match  => '.*timeout .*',
   }
 }
