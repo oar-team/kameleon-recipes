de1551ae3a712ad3bc5569f73ab6c779afa5747d
Teddy Valette <teddy.valette@inria.fr>
2019-10-02 13:44:38 +0200

    [helpers] create/export_appliance.py: fix tar xattrs option (capabilities) for buster
    Buster does not like the '
    
    Seems ok with Stretch as well without the '

--- steps/data/helpers/create_appliance.py
+++ steps/data/helpers/create_appliance.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 tar_options = ["--selinux",
                "--xattrs",
-               "--xattrs-include='*'",
+               "--xattrs-include=*",
                "--numeric-owner",
                "--one-file-system"]
 
