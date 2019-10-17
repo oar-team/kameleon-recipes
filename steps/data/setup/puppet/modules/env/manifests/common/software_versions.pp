877cfc4f57627aacd37069728bd267da03216860
Simon Delamare <simon.delamare@ens-lyon.fr>
2019-10-17 17:12:22 +0200

    [common] Update g5k-checks to 0.8.15

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -4,6 +4,6 @@ class env::common::software_versions {
    $g5k_subnets = '1.4.2'
    $g5k_meta_packages = '0.7.24'
    $tgz_g5k = '2.0.5'
-   $g5k_checks = '0.8.14'
+   $g5k_checks = '0.8.15'
    $sudo_g5k = '1.5'
 }
