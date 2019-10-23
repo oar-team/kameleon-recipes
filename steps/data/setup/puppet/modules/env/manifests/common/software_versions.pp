47d58e9b448f8ccfbb37640897eb84b445d1051c
Teddy Valette <teddy.valette@inria.fr>
2019-10-23 10:55:02 +0200

    Update g5k_meta_packages to 0.7.26

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -2,7 +2,7 @@
 
 class env::common::software_versions {
    $g5k_subnets = '1.4.2'
-   $g5k_meta_packages = '0.7.25'
+   $g5k_meta_packages = '0.7.26'
    $tgz_g5k = '2.0.5'
    $g5k_checks = '0.8.16'
    $sudo_g5k = '1.5'
