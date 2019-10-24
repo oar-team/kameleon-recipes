03583caa57aebc0a546c239e750a6a92a38f0452
Teddy Valette <teddy.valette@inria.fr>
2019-10-24 10:53:44 +0200

    [env::min::install_and_configure_ssh] remove useless changes from ssh config - remove deprecated options - remove options with the default value

--- steps/data/setup/puppet/modules/env/manifests/min/install_and_configure_ssh.pp
+++ steps/data/setup/puppet/modules/env/manifests/min/install_and_configure_ssh.pp
@@ -42,12 +42,9 @@ class env::min::install_and_configure_ssh {
 
   augeas {
     'sshd_config_min':
+      incl    => '/etc/ssh/sshd_config',
+      lens    => 'Sshd.lns',
       changes => [
-        'set /files/etc/ssh/sshd_config/HostKey[1] /etc/ssh/ssh_host_rsa_key',
-        'set /files/etc/ssh/sshd_config/HostKey[2] /etc/ssh/ssh_host_ecdsa_key',
-        'set /files/etc/ssh/sshd_config/HostKey[3] /etc/ssh/ssh_host_ed25519_key',
-        'set /files/etc/ssh/sshd_config/UsePrivilegeSeparation no',
-        'set /files/etc/ssh/sshd_config/PermitRootLogin without-password',
         'set /files/etc/ssh/sshd_config/PermitUserEnvironment yes',
         'set /files/etc/ssh/sshd_config/MaxStartups 500'
       ],
