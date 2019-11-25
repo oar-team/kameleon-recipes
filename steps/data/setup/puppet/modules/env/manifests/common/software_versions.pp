8f92692475174f72717788f5bfa6b5d30974f8ac
David Loup <david.loup@inria.fr>
2019-11-25 16:56:58 +0100

    Change g5k-meta-packages to 0.7.27

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -2,7 +2,7 @@
 
 class env::common::software_versions {
    $g5k_subnets = '1.4.2'
-   $g5k_meta_packages = '0.7.26'
+   $g5k_meta_packages = '0.7.27'
    $tgz_g5k = '2.0.6'
    $g5k_checks = '0.8.19'
    $sudo_g5k = '1.5'
