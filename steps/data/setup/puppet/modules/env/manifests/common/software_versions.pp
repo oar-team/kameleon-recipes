3aa3712bb62788280dacf668609059de4e7e6240
Samir Noir <samir.noir@inria.fr>
2020-01-16 14:29:20 +0100

    update g5k-checks to 0.8.24

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -4,6 +4,6 @@ class env::common::software_versions {
    $g5k_subnets = '1.4.2'
    $g5k_meta_packages = '0.7.29'
    $tgz_g5k = '2.0.6'
-   $g5k_checks = '0.8.23'
+   $g5k_checks = '0.8.24'
    $sudo_g5k = '1.5'
 }
