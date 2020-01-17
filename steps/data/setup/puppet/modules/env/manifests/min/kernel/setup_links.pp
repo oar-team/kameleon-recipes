3b430937ce2cdb31b536c4cccecf5f5591d650e7
Samir Noir <samir.noir@inria.fr>
2020-01-17 15:48:52 +0100

    Update min recipe to ensure kernel and initrd symlink are installed in /

--- steps/data/setup/puppet/modules/env/manifests/min/kernel/setup_links.pp
+++ steps/data/setup/puppet/modules/env/manifests/min/kernel/setup_links.pp
@@ -1,12 +1,33 @@
 class env::min::kernel::setup_links {
+  # Ensure the kernel symlink will be installed in /
+  file_line {
+    '/etc/kernel-img.conf: update kernel symlink behavior to link in /':
+      path     => '/etc/kernel-img.conf',
+      line     => "link_in_boot = no",
+      match    => '^link_in_boot =',
+      before   => Exec['linux-update-symlinks'];
+  }
+
+  # Ensure symlinks to /boot are removed
+  file {
+    '/boot/vmlinuz':
+      path     => '/boot/vmlinuz',
+      ensure   => absent;
+    '/boot/vmlinuz.old':
+      path     => '/boot/vmlinuz.old',
+      ensure   => absent;
+    '/boot/initrd.img':
+      path     => '/boot/initrd.img',
+      ensure   => absent;
+    '/boot/initrd.img.old':
+      path     => '/boot/initrd.img.old',
+      ensure   => absent;
+  }
 
   # Setup symlink for initrd and vmlinuz
-  file {
-    '/initrd.img':
-      ensure    => link,
-      target    => "/boot/initrd.img-${kernelrelease}";
-    '/vmlinuz':
-      ensure    => link,
-      target    => "/boot/vmlinuz-${kernelrelease}";
+  exec {
+    'linux-update-symlinks':
+      command => "/usr/bin/linux-update-symlinks install ${kernelrelease} /boot/vmlinuz-${kernelrelease}";
   }
+
 }
