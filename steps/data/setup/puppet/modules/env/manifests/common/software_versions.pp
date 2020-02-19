0022aecaed0395b6cdf1cfee06f99f0934723274
Albin PETIT <Albin.Petit@imag.fr>
2020-02-19 10:30:37 +0100

    Upgrade tgz-g5k to 2.0.9

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -3,7 +3,7 @@
 class env::common::software_versions {
    $g5k_subnets = '1.4.2'
    $g5k_meta_packages = '0.7.33'
-   $tgz_g5k = '2.0.6'
+   $tgz_g5k = '2.0.9'
    $g5k_checks = '0.8.26'
    $sudo_g5k = '1.5'
 }
