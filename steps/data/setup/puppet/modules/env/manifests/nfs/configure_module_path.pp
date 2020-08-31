05dd94c4a2f3ff0cab7ef7c153c52a66e2da41b0
Simon Delamare <simon.delamare@ens-lyon.fr>
2020-08-31 11:43:47 +0200

    [nfs] Configure module path for lmod

--- steps/data/setup/puppet/modules/env/manifests/nfs/configure_module_path.pp
+++ steps/data/setup/puppet/modules/env/manifests/nfs/configure_module_path.pp
@@ -2,7 +2,7 @@ class env::nfs::configure_module_path () {
 
   # Configure module path (installed in g5k-metapackage)
   file {
-    '/etc/environment-modules/modulespath':
+    '/etc/lmod/modulespath':
       ensure   => file,
       backup   => '.puppet-bak',
       content  => "/grid5000/spack/share/spack/modules/linux-debian9-x86_64\n/grid5000/spack/share/spack/modules/linux-debian10-x86_64\n",
