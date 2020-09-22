1323ae399aae192b01e9b884d2df8017fe5dc55f
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2020-09-22 17:32:29 +0200

    [std] Add libguestfs backports for ppc64el in addition to arm64
    Also change the name of the repository (from `libguestfs-backport-arm64`
    to simply `libguestfs-backport`)

--- steps/data/setup/puppet/modules/env/manifests/std/install_libguestfs_backport.pp
+++ steps/data/setup/puppet/modules/env/manifests/std/install_libguestfs_backport.pp
@@ -1,20 +1,27 @@
 class env::std::install_libguestfs_backport {
 
-  if $env::deb_arch == 'arm64' {
-    case $lsbdistcodename {
-      'buster': {
+  case $lsbdistcodename {
+    'buster': {
+      if $env::deb_arch == 'arm64' {
         env::common::g5kpackages {
-         'libguestfs-backport-arm64':
+         'libguestfs-backport':
            packages => 'libguestfs-tools',
            ensure  => $::env::common::software_versions::libguestfs_backport_arm64;
         }
       }
-      default: {
-        err "${lsbdistcodename} not supported."
+      elsif $env::deb_arch == 'ppc64el' {
+        env::common::g5kpackages {
+         'libguestfs-backport':
+           packages => 'libguestfs-tools',
+           ensure  => $::env::common::software_versions::libguestfs_backport_ppc64el;
+        }
+      }
+      else {
+        err "${env::deb_arch} not supported"
       }
     }
-  }
-  else {
-    err "${env::deb_arch} not supported"
+    default: {
+      err "${lsbdistcodename} not supported."
+    }
   }
 }
