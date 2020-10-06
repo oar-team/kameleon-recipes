511320fda47f6510e48e10b74b47ca00745ebf41
Alexandre MERLIN <alexandre.merlin@inria.fr>
2020-10-06 10:09:16 +0200

    [common] Update sudo-g5k to v1.9

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -5,7 +5,7 @@ class env::common::software_versions {
    $g5k_meta_packages = '0.7.34'
    $tgz_g5k = '2.0.12'
    $g5k_checks = '0.9.2'
-   $sudo_g5k = '1.8'
+   $sudo_g5k = '1.9'
    $libguestfs_backport_arm64 = '1:1.40.2-7~bpog5k10+1'
    $ganglia_monitor = '3.6.0-7.1'
 }
