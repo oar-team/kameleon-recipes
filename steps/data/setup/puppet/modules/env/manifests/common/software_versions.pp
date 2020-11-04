48348fd31abbd5633c3e9a702e53826aa9e26791
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2020-11-04 10:41:50 +0100

    [puppet] Update g5k-checks to 0.10.2: POWER support

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -4,7 +4,7 @@ class env::common::software_versions {
    $g5k_subnets = '1.4.2'
    $g5k_meta_packages = '0.7.36'
    $tgz_g5k = '2.0.12'
-   $g5k_checks = '0.9.2'
+   $g5k_checks = '0.10.2'
    $sudo_g5k = '1.9'
    $libguestfs_backport_arm64 = '1:1.40.2-7~bpog5k10+1'
    $ganglia_monitor = '3.6.0-7.1'
