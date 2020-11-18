3e07114ad227ae416ff3175815ad38c6237e6a77
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2020-11-18 18:12:39 +0100

    [big] [puppet] [nvidia] Ensure Puppet fails if nvidia driver installation fails

--- steps/data/setup/puppet/modules/env/manifests/big/configure_nvidia_gpu/drivers.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/configure_nvidia_gpu/drivers.pp
@@ -29,10 +29,14 @@ class env::big::configure_nvidia_gpu::drivers () {
       user      => root,
       require   => Package['module-assistant'];
     'install_nvidia_driver':
-      command   => "/tmp/NVIDIA-Linux.run -qa --no-cc-version-check --ui=none --dkms -k ${installed_kernelreleases[-1]}; /bin/rm /tmp/NVIDIA-Linux.run",
+      command   => "/tmp/NVIDIA-Linux.run -qa --no-cc-version-check --ui=none --dkms -k ${installed_kernelreleases[-1]}",
       timeout   => 1200, # 20 min,
       user      => root,
       require   => [Exec['prepare_kernel_module_build'], File['/tmp/NVIDIA-Linux.run'], Package['dkms']];
+    'cleanup_nvidia':
+      command   => "/bin/rm /tmp/NVIDIA-Linux.run",
+      user      => root,
+      require   => Exec['install_nvidia_driver'];
   }
   file{
     '/tmp/NVIDIA-Linux.run':
