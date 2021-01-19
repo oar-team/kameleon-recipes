27ffc4833b488531e0edf5177057a92cf916c95c
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-01-19 15:09:45 +0100

    [big][amd64] Add g5k-jupyterlab package

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -2,7 +2,7 @@
 
 class env::common::software_versions {
    $g5k_subnets = '1.4.2'
-   $g5k_meta_packages = '0.7.38'
+   $g5k_meta_packages = '0.7.40'
    $tgz_g5k = '2.0.12'
    $g5k_checks = '0.10.6'
    $sudo_g5k = '1.9'
@@ -11,4 +11,5 @@ class env::common::software_versions {
    $libguestfs_backport_ppc64el = '1:1.40.2-7~bpog5k10+1'
    $lmod = '6.6-0.3g5k1'
    $datacenter_gpu_manager = '1:1.7.2'
+   $g5k_jupyterlab = '0.4'
 }
