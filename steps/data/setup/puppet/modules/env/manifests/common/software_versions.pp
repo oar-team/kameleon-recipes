4a009bbe556f933ca9b92cb8994df6134ed09ff6
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-01-20 12:08:17 +0100

    [common] Update g5k-meta-packages to v0.7.44

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -2,7 +2,7 @@
 
 class env::common::software_versions {
    $g5k_subnets = '1.4.2'
-   $g5k_meta_packages = '0.7.40'
+   $g5k_meta_packages = '0.7.44'
    $tgz_g5k = '2.0.14'
    $g5k_checks = '0.10.6'
    $sudo_g5k = '1.9'
