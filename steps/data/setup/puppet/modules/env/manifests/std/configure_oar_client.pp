c31a6cc6c3cbe8c97f58799ca568ca07efb0f4f5
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-02-10 15:28:20 +0100

    [debian11] Use sid_beta OAR repository

--- steps/data/setup/puppet/modules/env/manifests/std/configure_oar_client.pp
+++ steps/data/setup/puppet/modules/env/manifests/std/configure_oar_client.pp
@@ -15,6 +15,15 @@ class env::std::configure_oar_client {
         $oar_repos         = "2.5/debian/";
         $oar_repos_release = "buster-backports_beta"
       }
+      'bullseye' : {
+        # FIXME Beta pour le moment
+        $oar_version       = "2.5.9-1";
+        $oar_repos         = "debian/";
+        $oar_repos_release = "sid_beta"
+      }
+      default : {
+        err "${::lsbdistcodename} not supported."
+      }
     }
   }
 
