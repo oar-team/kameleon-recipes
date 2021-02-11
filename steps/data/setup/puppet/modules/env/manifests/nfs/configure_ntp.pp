734a8b7e5658b506084e28ae94344b5fef9c42f9
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-02-11 16:21:53 +0100

    Remove jessie from ntp recipe

--- steps/data/setup/puppet/modules/env/manifests/nfs/configure_ntp.pp
+++ steps/data/setup/puppet/modules/env/manifests/nfs/configure_ntp.pp
@@ -1,79 +1,40 @@
 class env::nfs::configure_ntp ( $drift_file = false ) {
 
+  $ntp = [ 'ntp', 'ntpdate' ]
+  package {
+    'ntpdate':
+      ensure    => installed;
+    'ntp':
+      ensure    => installed,
+      require   => Package['openntpd'];
+    'openntpd':
+      ensure    => absent;
+  } # Here we forced ntp package to be 'ntp' and not 'openntp' because ntp is listed as dependencie by g5kchecks and conflict openntp.
 
-  if "${::lsbdistcodename}" == "jessie" {
+  file {
+    '/etc/ntp.conf':
+      ensure    => file,
+      owner     => root,
+      group     => root,
+      mode      => '0644',
+      content   => template("env/nfs/ntp/ntp.conf.erb"),
+      notify    => Service['ntp'];
+  }
 
-    $ntp = [ 'openntpd', 'ntpdate' ]
-    package {
-      'ntpdate':
-        ensure    => installed;
-      'ntp':
-        ensure    => absent,
-        require   => Service['ntp'];
-      'openntpd':
-        ensure    => installed,
-        require   => Package['ntp'];
-    }
-
+  if $drift_file {
     file {
-      '/etc/ntp.conf':
+      '/var/lib/ntp/ntp.drift':
         ensure    => file,
-        owner     => root,
-        group     => root,
+        owner     => ntp,
+        group     => ntp,
         mode      => '0644',
-        content   => template("env/nfs/ntp/ntp.conf.erb"),
-        notify    => Service['openntpd'];
+        content   => "",
+        require   => Package[$ntp];
     }
+  }
 
-    service {
-      'ntp':
-        ensure    => stopped,
-        enable    => false;
-    }
-
-    service {
-      'openntpd':
-        enable    => true;
-    }
-
-  } else {
-
-    $ntp = [ 'ntp', 'ntpdate' ]
-    package {
-      'ntpdate':
-        ensure    => installed;
-      'ntp':
-        ensure    => installed,
-        require   => Package['openntpd'];
-      'openntpd':
-        ensure    => absent;
-    }# Here we forced ntp package to be 'ntp' and not 'openntp' because ntp is listed as dependencie by g5kchecks and conflict openntp.
-
-    file {
-      '/etc/ntp.conf':
-        ensure    => file,
-        owner     => root,
-        group     => root,
-        mode      => '0644',
-        content   => template("env/nfs/ntp/ntp.conf.erb"),
-        notify    => Service['ntp'];
-    }
-
-    if $drift_file {
-      file {
-        '/var/lib/ntp/ntp.drift':
-          ensure    => file,
-          owner     => ntp,
-          group     => ntp,
-          mode      => '0644',
-          content   => "",
-          require   => Package[$ntp];
-      }
-    }
-
-    service {
-      'ntp':
-        enable    => true;
-    }
+  service {
+    'ntp':
+      enable    => true;
   }
 }
