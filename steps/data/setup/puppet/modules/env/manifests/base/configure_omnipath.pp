2a9c54bb94582b890089433ab4bcc9308945fcce
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-03-31 10:09:43 +0200

    [debian11] Add OPA configuration

--- steps/data/setup/puppet/modules/env/manifests/base/configure_omnipath.pp
+++ steps/data/setup/puppet/modules/env/manifests/base/configure_omnipath.pp
@@ -1,13 +1,38 @@
 class env::base::configure_omnipath(){
 
-
   case "${::lsbdistcodename}" {
+    'bullseye': {
+      $opapackages = ['opa-address-resolution', 'opa-fastfabric', 'libopamgt0', 'libopasadb1',
+                      'opa-basic-tools', 'firmware-misc-nonfree']
+
+      $rdmapackages = ['qperf', 'libibverbs1', 'librdmacm1', 'libibmad5', 'libibumad3', 'ibverbs-providers',
+                      'rdmacm-utils', 'infiniband-diags', 'libfabric1', 'ibverbs-utils']
+
+      if $env::deb_arch == 'amd64' {
+        ensure_packages([$opapackages, $rdmapackages], {
+          ensure => present
+        })
+
+        # rdma-load-modules@opa.service would fail with opa_vnic (not available)
+        # opa_vnic isn't required to make OPA working
+        exec {
+          'disable opa_vnic':
+            command => "/bin/sed -i 's/opa_vnic/# opa_vnic/g' /etc/rdma/modules/opa.conf",
+            require => Package[$rdmapackages]
+        }
+      } else {
+        # opapackages are only available on amd64
+        ensure_packages($rdmapackages, {
+          ensure => present
+        })
+      }
+    }
     'buster': {
       $opapackages = ['opa-address-resolution', 'opa-fastfabric', 'libopamgt0', 'libopasadb1',
                       'opa-basic-tools', 'firmware-misc-nonfree']
 
       $rdmapackages = ['qperf', 'libibverbs1', 'librdmacm1', 'libibmad5', 'libibumad3', 'ibverbs-providers',
-                      'rdmacm-utils', 'rdmacm-utils', 'infiniband-diags', 'libfabric1', 'ibverbs-utils']
+                      'rdmacm-utils', 'infiniband-diags', 'libfabric1', 'ibverbs-utils']
 
       if $env::deb_arch == 'amd64' {
         ensure_packages([$opapackages, $rdmapackages], {
