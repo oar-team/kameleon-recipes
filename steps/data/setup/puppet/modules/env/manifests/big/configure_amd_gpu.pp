a8c3a59c08ee600a1f009af81b07a56d69324018
Nicolas Perrin <nicolas.perrin@inria.fr>
2021-06-07 11:03:16 +0200

    [debian11][amd] remove amd gpu stuff bug #13159

--- steps/data/setup/puppet/modules/env/manifests/big/configure_amd_gpu.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/configure_amd_gpu.pp
@@ -1,34 +1,42 @@
 class env::big::configure_amd_gpu () {
 
-  apt::source {
-    'repo.radeon.com':
-      comment      => 'Repo for AMD ROCM packages',
-      location     => 'https://repo.radeon.com/rocm/apt/debian/',
-      release      => 'xenial',
-      repos        => 'main',
-      architecture => 'amd64',
-      key          => {
-        'id'     => '1A693C5C',
-        'source' => 'https://repo.radeon.com/rocm/rocm.gpg.key',
-      },
-      include      => {
-        'deb' => true,
-        'src' => false
-      },
-      notify       => Exec['apt_update'],
-  }
+  case "${::lsbdistcodename}" {
 
-  package {
-    [ 'rock-dkms', 'hip-base', 'rocminfo', 'rocm-smi-lib', 'hip-rocclr', 'rocm-device-libs' ]:
-      ensure          => installed,
-      install_options => ['--no-install-recommends'],
-      require         => [Apt::Source['repo.radeon.com'], Exec['apt_update']];
-  }
+    "buster" : {
+      apt::source {
+        'repo.radeon.com':
+          comment      => 'Repo for AMD ROCM packages',
+          location     => 'https://repo.radeon.com/rocm/apt/debian/',
+          release      => 'xenial',
+          repos        => 'main',
+          architecture => 'amd64',
+          key          => {
+            'id'     => '1A693C5C',
+            'source' => 'https://repo.radeon.com/rocm/rocm.gpg.key',
+          },
+          include      => {
+            'deb' => true,
+            'src' => false
+          },
+          notify       => Exec['apt_update'],
+      }
 
-  file_line {
-    'rocm_etc_profile_path':
-      path => '/etc/profile',
-      line => 'export PATH=$PATH:/opt/rocm-4.2.0/bin';
-  }
+      package {
+        [ 'rock-dkms', 'hip-base', 'rocminfo', 'rocm-smi-lib', 'hip-rocclr', 'rocm-device-libs' ]:
+          ensure          => installed,
+          install_options => ['--no-install-recommends'],
+          require         => [Apt::Source['repo.radeon.com'], Exec['apt_update']];
+      }
 
+      file_line {
+        'rocm_etc_profile_path':
+          path => '/etc/profile',
+          line => 'export PATH=$PATH:/opt/rocm-4.2.0/bin';
+      }
+    }
+
+    "bullseye" : {
+      # TODO Build du module amdgpu (Rocm 4.2) en erreur avec le kernel 5.10 - Bug #13159
+    }
+  }
 }
