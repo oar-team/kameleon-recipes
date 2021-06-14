9bca1c84af31f2276d0e564e7b43074b2fda554e
Simon Delamare <simon.delamare@ens-lyon.fr>
2021-06-14 16:44:34 +0200

    [software] Update g5k-checks to 0.11.1

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -4,7 +4,7 @@ class env::common::software_versions {
    $g5k_subnets = '1.4.2'
    $g5k_meta_packages = '0.7.45'
    $tgz_g5k = '2.0.17'
-   $g5k_checks = '0.11.0'
+   $g5k_checks = '0.11.1'
    $sudo_g5k = '1.11'
    $ganglia_monitor = '3.6.0-7.1'
    $libguestfs_backport_arm64 = '1:1.40.2-7~bpog5k10+1'
