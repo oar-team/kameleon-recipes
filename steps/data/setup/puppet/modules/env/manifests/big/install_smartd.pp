371e4944030f90f269bdb7d7363d05928a1ac410
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2021-09-08 14:20:21 +0200

    [big] Disable smartd on debian9: it does not work, and there was no need to add it there

--- steps/data/setup/puppet/modules/env/manifests/big/install_smartd.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/install_smartd.pp
@@ -1,26 +1,28 @@
 class env::big::install_smartd {
 
-  package {
-    'smartmontools':
-      ensure => installed;
-  }
+  if "$::lsbdistcodename" != "stretch" {
 
-  file {
-    '/etc/systemd/system/smartd.service.d/':
-      ensure  => directory,
-      require => Package['smartmontools'];
-    '/etc/systemd/system/smartd.service.d/override.conf':
+    package {
+      'smartmontools':
+        ensure => installed;
+    }
+
+    file {
+      '/etc/systemd/system/smartd.service.d/':
+        ensure  => directory,
+        require => Package['smartmontools'];
+      '/etc/systemd/system/smartd.service.d/override.conf':
+        ensure  => present,
+        content => "[Service]\nExecStartPre=mkdir -p /dev/discs",
+        require => File['/etc/systemd/system/smartd.service.d/'];
+    }
+
+    file_line { 'smard.conf':
       ensure  => present,
-      content => "[Service]\nExecStartPre=mkdir -p /dev/discs",
-      require => File['/etc/systemd/system/smartd.service.d/'];
+      require => Package['smartmontools'],
+      path    => '/etc/smartd.conf',
+      line    => 'DEVICESCAN -d nvme -d scsi -d ata -d sat -n standby -m root -M exec /usr/share/smartmontools/smartd-runner',
+      match   => '^DEVICESCAN .*';
+    }
   }
-
-  file_line { 'smard.conf':
-    ensure  => present,
-    require => Package['smartmontools'],
-    path    => '/etc/smartd.conf',
-    line    => 'DEVICESCAN -d nvme -d scsi -d ata -d sat -n standby -m root -M exec /usr/share/smartmontools/smartd-runner',
-    match   => '^DEVICESCAN .*';
-  }
-
 }
