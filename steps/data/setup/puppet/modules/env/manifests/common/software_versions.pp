62f248bf8345a5863911ac877010051fe242586b
Patrice RINGOT <patrice.ringot@loria.fr>
2021-09-28 13:45:35 +0200

    [software] update g5k-meta-packages to 0.7.46 (bug 13383)

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -2,7 +2,7 @@
 
 class env::common::software_versions {
   $g5k_subnets                 = '1.4.2'
-  $g5k_meta_packages           = '0.7.45'
+  $g5k_meta_packages           = '0.7.46'
   $tgz_g5k                     = '2.0.17'
   $g5k_checks                  = '0.11.2'
   $sudo_g5k                    = '1.11'
