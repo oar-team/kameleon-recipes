871568a8459a446bbd3c1d797ff109b6570db57f
Baptiste Jonglez <baptiste.jonglez@imag.fr>
2021-11-24 17:21:40 +0100

    [nfs] [modules] Remove debian9 modules by default on debian11 (bug 13590)

--- steps/data/setup/puppet/modules/env/manifests/nfs/configure_module_path.pp
+++ steps/data/setup/puppet/modules/env/manifests/nfs/configure_module_path.pp
@@ -3,7 +3,15 @@ class env::nfs::configure_module_path () {
   # Configure module path (installed in g5k-metapackage)
   case "$env::deb_arch" {
     "amd64": {
-      $modulespath = "/grid5000/spack/share/spack/modules/linux-debian9-x86_64\n/grid5000/spack/share/spack/modules/linux-debian10-x86_64\n"
+      case "${::lsbdistcodename}" {
+        "bullseye" : {
+          # Bug 13590
+          $modulespath = "/grid5000/spack/share/spack/modules/linux-debian10-x86_64\n"
+        }
+        default: {
+          $modulespath = "/grid5000/spack/share/spack/modules/linux-debian9-x86_64\n/grid5000/spack/share/spack/modules/linux-debian10-x86_64\n"
+        }
+      }
     }
     "ppc64el": {
       $modulespath = "/grid5000/spack/share/spack/modules/linux-debian10-ppc64le\n"
