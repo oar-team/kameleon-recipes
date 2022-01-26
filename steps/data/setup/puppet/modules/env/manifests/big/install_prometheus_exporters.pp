a8ef9e75f6aad188138be570313e7e6cab38c7a0
Patrice RINGOT <patrice.ringot@loria.fr>
2022-01-26 17:43:59 +0100

    [prometheus] disable the collection of NFS filesystems

--- steps/data/setup/puppet/modules/env/manifests/big/install_prometheus_exporters.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/install_prometheus_exporters.pp
@@ -5,4 +5,13 @@ class env::big::install_prometheus_exporters {
       ensure => installed;
   }
 
+  file {
+    '/etc/default/prometheus-node-exporter':
+      require => Package['prometheus-node-exporter'],
+      content => file('env/big/prometheus/prometheus-node-exporter.default'),
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+  }
+
 }
