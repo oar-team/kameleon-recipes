fe04e8da3a141fca0501867385db777d21ba0747
Patrice RINGOT <patrice.ringot@loria.fr>
2022-03-03 15:03:29 +0100

    Use autofs-g5k for bullseye (bug 13638)

--- steps/data/setup/puppet/modules/env/manifests/nfs/install_osirim_requirements.pp
+++ steps/data/setup/puppet/modules/env/manifests/nfs/install_osirim_requirements.pp
@@ -1,42 +1,51 @@
 class env::nfs::install_osirim_requirements () {
 
-  package {
-    'autofs':
-      ensure   => installed;
+  if ($::lsbdistcodename == 'bullseye') {
+    env::common::g5kpackages {
+      'autofs-g5k':
+        ensure   => '5.1.2-4',
+        packages => ['autofs'],
+        release  => $::lsbdistcodename;
+    }
+  } else {
+    package {
+      'autofs':
+        ensure => installed;
+    }
   }
 
   service {
     'autofs':
-      ensure   => running,
-      require  => Package['autofs'];
+      ensure  => running,
+      require => Package['autofs'];
   }
 
   file {
     '/srv/osirim':
-      ensure   => directory,
-      owner    => root,
-      group    => root,
-      mode     => '0755';
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => '0755';
     '/etc/auto.master.d':
-      ensure   => directory,
-      owner    => root,
-      group    => root,
-      mode     => '0755';
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => '0755';
     '/etc/auto.master.d/osirim.autofs':
-      ensure   => file,
-      owner    => root,
-      group    => root,
-      mode     => '0644',
-      content  => '/srv/osirim /etc/auto.osirim --timeout=60',
-      require  => File['/etc/auto.master.d'],
-      notify   => Service['autofs'];
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => '/srv/osirim /etc/auto.osirim --timeout=60',
+      require => File['/etc/auto.master.d'],
+      notify  => Service['autofs'];
     '/etc/auto.osirim':
-      ensure   => file,
-      owner    => root,
-      group    => root,
-      mode     => '0644',
-      content  => '* -fstype=nfs,rw,nfsvers=3,hard,intr,async,noatime,nodev,nosuid,auto,rsize=32768,wsize=32768 osirim.toulouse.grid5000.fr:/ifs/grid5000/data/home/&',
-      require  => File['/srv/osirim'],
-      notify   => Service['autofs'];
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => '* -fstype=nfs,rw,nfsvers=3,hard,intr,async,noatime,nodev,nosuid,auto,rsize=32768,wsize=32768 osirim.toulouse.grid5000.fr:/ifs/grid5000/data/home/&',
+      require => File['/srv/osirim'],
+      notify  => Service['autofs'];
   }
 }
