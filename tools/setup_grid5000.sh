f44a8b1347ac592d6edcc8aa7e8256b3a1b8376d
Nicolas Perrin <nicolas.perrin@inria.fr>
2022-03-16 09:13:58 +0100

    [tools][setup_grid5000] Revert temporary workaround

--- tools/setup_grid5000.sh
+++ tools/setup_grid5000.sh
@@ -12,24 +12,19 @@ echo "mdadm   mdadm/initrdstart       string  none" | debconf-set-selections
 # echo 'deb [check-valid-until=no] http://snapshot.debian.org/archive/debian-security/20190813T053201Z/ buster/updates main' > /etc/apt/sources.list.d/snapshot-kernel.list
 
 KERNEL=linux-image-$(uname -r)
-# Temporary unavailable packages fix
-#VERSION=$(apt-cache policy $KERNEL | grep Installed: | awk '{print $2}')
+VERSION=$(apt-cache policy $KERNEL | grep Installed: | awk '{print $2}')
 ARCH=$(dpkg --print-architecture)
 KERNEL_SHORT=$(uname -r | sed -re "s/^(.*)-[^-]*/\1/g")
 apt-get update
-#apt-get install -y systemtap linux-image-$(uname -r)-dbg=$VERSION linux-headers-$(uname -r)=$VERSION linux-headers-$KERNEL_SHORT-common=$VERSION
-#/tmp/environments-recipes/tools/nofsync.stp </dev/null >/dev/null 2>&1 &
+apt-get install -y systemtap linux-image-$(uname -r)-dbg=$VERSION linux-headers-$(uname -r)=$VERSION linux-headers-$KERNEL_SHORT-common=$VERSION
+/tmp/environments-recipes/tools/nofsync.stp </dev/null >/dev/null 2>&1 &
 
 # if arm64 or ppc64, use backported package for libguestfs-tools. see #11432
 if [ "$ARCH" = "arm64" -o "$ARCH" = "ppc64el" ]; then
 	echo deb http://packages.grid5000.fr/deb/libguestfs-backport / > /etc/apt/sources.list.d/libguestfs-backport.list
 fi
 # install other dependencies
-if [ "$ARCH" = "arm64" ]; then
-  apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends netcat eatmydata libguestfs-tools gnupg-agent
-else
-  apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends linux-headers-$(uname -r) netcat eatmydata libguestfs-tools gnupg-agent
-fi
+apt-get update && DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends linux-headers-$(uname -r) netcat eatmydata libguestfs-tools gnupg-agent
 
 mv /bin/gzip /bin/gzip.OLD
 ln -s /usr/bin/pigz /bin/gzip
