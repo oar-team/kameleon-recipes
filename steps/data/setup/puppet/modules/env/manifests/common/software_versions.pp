516b87170aa305f417c3f3122a4a4e0a965c530c
Alexandre MERLIN <alexandre.merlin@inria.fr>
2022-05-03 14:38:15 +0200

    [software] update sudo-g5k to v1.13

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -5,7 +5,7 @@ class env::common::software_versions {
   $g5k_meta_packages           = '0.7.49'
   $tgz_g5k                     = '2.0.19'
   $g5k_checks                  = '0.11.9'
-  $sudo_g5k                    = '1.12'
+  $sudo_g5k                    = '1.13'
   $ganglia_monitor             = '3.6.0-7.1'
   $libguestfs_backport_arm64   = '1:1.40.2-7~bpog5k10+1'
   $libguestfs_backport_ppc64el = '1:1.40.2-7~bpog5k10+1'
