cba33ff6119ae5fd161473206bdbf572fd3c637a
Samir Noir <samir.noir@inria.fr>
2022-05-12 13:41:19 +0200

    [software] update g5k-checks to 0.11.10

--- steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
+++ steps/data/setup/puppet/modules/env/manifests/common/software_versions.pp
@@ -4,7 +4,7 @@ class env::common::software_versions {
   $g5k_subnets                 = '1.4.2'
   $g5k_meta_packages           = '0.7.49'
   $tgz_g5k                     = '2.0.19'
-  $g5k_checks                  = '0.11.9'
+  $g5k_checks                  = '0.11.10'
   $sudo_g5k                    = '1.13'
   $ganglia_monitor             = '3.6.0-7.1'
   $libguestfs_backport_arm64   = '1:1.40.2-7~bpog5k10+1'
