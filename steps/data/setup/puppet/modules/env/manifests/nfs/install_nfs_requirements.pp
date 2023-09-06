df42fa7d182908b41f9e3872c4fe20e80c128480
Nicolas Perrin <nicolas.perrin@inria.fr>
2023-09-06 16:05:10 +0200

    [debian11][nfs] remove forcing python3 version as the recommended version in nfs-common package is fixed

--- steps/data/setup/puppet/modules/env/manifests/nfs/install_nfs_requirements.pp
+++ steps/data/setup/puppet/modules/env/manifests/nfs/install_nfs_requirements.pp
@@ -6,21 +6,4 @@ class env::nfs::install_nfs_requirements () {
     'libcap2-bin':
       ensure   => installed;
   }
-
-  case "${::lsbdistcodename}" {
-    "buster": {
-      # NOTHING
-    }
-    "bullseye" : {
-      # Force python3 usage instead of python2 (only 'python' is Recommended by nfs-common package)
-      # see bug #13194
-      package {
-        'python-is-python3':
-          ensure => installed;
-      }
-    }
-    default : {
-      fail "${::lsbdistcodename} not supported."
-    }
-  }
 }
