5112f6f0cfb64d73bcf9fdbc1a62394afbf6d117
Pierre Neyron <pierre.neyron@imag.fr>
2023-10-24 00:15:49 +0200

    Fix kaenv-customize helper

--- steps/data/helpers/kaenv-customize.py
+++ steps/data/helpers/kaenv-customize.py
@@ -14,7 +14,7 @@ class Environment:
     desc = None
     def __init__(self):
         """initialize an Environment object"""
-        
+
     def import_from_kaenv(self, env, remote = None):
         """import a kadeploy environment from	a kadeploy database"""
         env = re.match(r"^(?P<name>[-_.\w]+)(?:@(?P<user>[_.\w]+))?(:?:(?P<version>[_.\w]+))?(:?%(?P<arch>[_.\w]+))?$", env).groupdict("")
@@ -30,17 +30,25 @@ class Environment:
                 p = subprocess.Popen(["ssh", remote ] + kaenv_cmd.split(), stdout=subprocess.PIPE)
             else:
                 p = subprocess.Popen(kaenv_cmd.split(), stdout=subprocess.PIPE)
-            self.desc = yaml.load(p.stdout, Loader=yaml.SafeLoader)
+            docs = list(yaml.safe_load_all(p.stdout))
         except Exception as exc:
             raise Exception("Failed to import environment description with '{}'".format(kaenv_cmd))
+        if len(docs) > 1:
+            raise Exception("kaenv returned more than one environment, you may need to provide the architecture")
+        self.desc = docs[0]
         return self
-    
+
     def modify(self, **kwargs):
         for key, value in kwargs.items():
-            if key in ['author', 'description', 'filesystem', 'name', 'os', 'visibility']:
+            if key in ['author', 'description', 'filesystem', 'name', 'os', 'visibility', 'arch', 'alias']:
                 self.desc[key] = str(value)
-            elif key in ['destructive', 'multipart', 'visibility']:
-                self.desc[key] = bool(value)
+            elif key in ['destructive', 'multipart', 'visibility', 'kexec']:
+                if value.lower() == "true":
+                    self.desc[key] = True
+                elif value.lower() == "false":
+                    self.desc[key] = False
+                else:
+                    raise Exception("Invalid value for {}: {}".format(key, value))
             elif key in ['partition_type', 'version']:
                 self.desc[key] = int(value)
             else:
@@ -51,7 +59,7 @@ class Environment:
                 if ((key1 == 'boot' and key2 in ['initrd', 'kernel', 'kernel_params']) or
                     (key1 == 'image' and key2 in ['compression', 'file', 'kind'])):
                     self.desc[key1][key2] = value
-                elif (key1 == 'postinstalls' and key2[-1:].isdigit() and 
+                elif (key1 == 'postinstalls' and key2[-1:].isdigit() and
                       key2[:-1] in ['archive', 'compression', 'script']):
                     if self.desc[key1][int(key2[-1:])]:
                       self.desc[key1][int(key2[-1:])][key2[:-1]] = value
@@ -65,17 +73,31 @@ class Environment:
         self.desc['postinstalls'].append({'archive': archive, 'compression': compression, 'script': script})
         return self
 
-    def del_postinstall(self, index):
-        del(self.desc['postinstall'][index])
+    def del_postinstall(self, n):
+        n=min(int(n), len(self.desc['postinstalls']))
+        self.desc['postinstalls']=self.desc['postinstalls'][:-n]
+        return self
+
+    def add_variables(self, **kwargs):
+        for key, value in kwargs.items():
+            if 'custom_variables' in self.desc:
+                self.desc['custom_variables'][key] = value;
+            else:
+                self.desc['custom_variables'] = { key: value };
+        return self
+
+    def del_variable(self, key):
+        if 'custom_variables' in self.desc:
+            self.desc['custom_variables'].pop(key, None)
         return self
 
     def del_alias(self):
-        del(self.desc['alias'])
+        self.desc.pop('alias', None)
         return self
 
     def export(self):
         print("---\n" + yaml.dump(self.desc, default_flow_style=False))
-		
+
 logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
@@ -83,7 +105,11 @@ if __name__ == '__main__':
     parser.add_argument('environment', metavar="ENVIRONMENT", help='Environment to import')
     parser.add_argument('--remote', metavar="REMOTE", help='Remote kadeploy frontend')
     parser.add_argument('--modify', metavar="KEY=VALUE", action='append', type=lambda kv: kv.split("=",1), help='modify description')
-    parser.add_argument('--add-postinstall', metavar="ARCHIVE:COMPRESSION:SCRIPT", help='add an additional postinstall')
+    parser.add_argument('--add-postinstall', metavar="ARCHIVE:COMPRESSION:SCRIPT", action='append', help='add an additional postinstall')
+    parser.add_argument('--del-postinstall', action='count', help='delete the postinstall (the latest if more than one exist)')
+    parser.add_argument('--add-variable', metavar="KEY=VALUE", action='append', type=lambda kv: kv.split("=",1), help='add a custom variable')
+    parser.add_argument('--del-variable', metavar="KEY", action='append', help='delete a custom variable')
+    parser.add_argument('--del-alias', action='store_true', default=False, help='delete alias')
     parser.add_argument('--info', action="store_true", default=False, help='print info messages')
     parser.add_argument('--debug', action="store_true", default=False, help='print debug messages')
     args = parser.parse_args()
@@ -102,15 +128,28 @@ if __name__ == '__main__':
     logger.addHandler(handler)
 
     try:
-        if any(len(kv) < 2 for kv in args.modify):
-            raise ValueError('--modify takes an argument in the form "key=value"')
-        env = Environment().import_from_kaenv(args.environment, args.remote).modify(**dict(args.modify))
+        env = Environment().import_from_kaenv(args.environment, args.remote)
+        if args.modify is not None:
+            if any(len(kv) < 2 for kv in args.modify):
+                raise ValueError('--modify takes an argument in the form "key=value"')
+            env.modify(**dict(args.modify))
+        if args.del_postinstall:
+            env.del_postinstall(args.del_postinstall)
         if args.add_postinstall:
-            add_postinstall_args = re.match(r'^(.+):([^:]+):([^:]+)$', args.add_postinstall)
-            if not add_postinstall_args:
-                raise ValueError('--add-postinstall takes an argument in the form "path:compression:cmdline", got {}'.format(args.add_postinstall))
-            env.add_postinstall(*add_postinstall_args.groups())
-        env.del_alias()
+            for p in args.add_postinstall:
+                pp = re.match(r'^(.+):([^:]+):([^:]+)$', p)
+                if not pp:
+                    raise ValueError('--add-postinstall takes an argument in the form "path:compression:cmdline"')
+                env.add_postinstall(*pp.groups())
+        if args.del_variable:
+            for v in args.del_variable:
+                env.del_variable(v)
+        if args.add_variable is not None:
+            if any(len(kv) < 2 for kv in args.add_variable):
+                raise ValueError('--add_variable takes an argument in the form "key=value"')
+            env.add_variables(**dict(args.add_variable))
+        if args.del_alias:
+            env.del_alias()
         env.export()
     except Exception as exc:
         sys.stderr.write("Error: {}\n".format(exc))
