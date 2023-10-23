86145cc96c0fc67b0e655bda595af00f35f6166f
Pierre Neyron <pierre.neyron@imag.fr>
2023-10-24 00:33:01 +0200

    [puppet] Fix the postfix package (myhostname postinst bug with debian ≤ 11)
    Revert comment removal from old commit.

--- steps/data/setup/puppet/modules/env/manifests/big/configure_postfix.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/configure_postfix.pp
@@ -9,11 +9,24 @@ class env::big::configure_postfix () {
       before  => Exec['newaliases', 'set_root_alias'];
   }
 
+  if ($::lsbdistcodename != 'bookworm') {
+    exec {
+      # This is a damn dirty patch due to a bug in the debian package for
+      # postfix. In the package, the postinst script creates a
+      # /etc/postfix/main.cfg with a myhostname attribute which is retrieved by
+      # concatening the hostname found in /etc/hostname and domain extracted from
+      # /etc/resolv.conf.  This domain MAY have a trailing dot (.grid5000.fr.)
+      # Sadly, after this, newaliases is called on this /etc/postfix/main.cfg and
+      # doesn't digest any trailing dot for attribute myhostname.  This is fixed
+      # in Debian bookworm:
+      # https://salsa.debian.org/postfix-team/postfix-dev/-/commit/a1d904dc162dfb810fc5b44b0f3efc45b07c12f8
+      'fix_resolv_conf':
+        command  => "/bin/sed -e 's/\\.\\(\\s\\|$\\)/\\1/g' -i /etc/resolv.conf";
+    }
+  }
   exec {
-    'fix_resolv_conf':
-      command  => "/bin/sed 's/\\([^\\s]*\\)\\.\\(\\s\\|$\\)/\\1\\2/g' -i /etc/resolv.conf";
     'fix_hostname':
-      command  => "/bin/sed 's/localhost//' -i /etc/hostname";
+      command  => "/bin/sed -e 's/localhost//' -i /etc/hostname";
     # set root alias to local + internal mailbox
     'set_root_alias':
       command  => "if /usr/bin/grep -q ^root: /etc/aliases; then /bin/sed -i 's/^root:.*/root: root, ${g5k_sysemail}/' /etc/aliases; else /usr/bin/echo 'root: root, ${g5k_sysemail}' >> /etc/aliases; fi",
