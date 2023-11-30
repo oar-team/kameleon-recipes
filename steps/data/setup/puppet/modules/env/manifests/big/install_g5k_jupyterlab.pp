a7d31022b121bcc8e3464406a4a632f872b33da4
Yao CHI <yao.chi@inria.fr>
2023-11-30 14:47:57 +0100

    [debian12] not install g5k_jupyterlab for ppc64, see bug 14311

--- steps/data/setup/puppet/modules/env/manifests/big/install_g5k_jupyterlab.pp
+++ steps/data/setup/puppet/modules/env/manifests/big/install_g5k_jupyterlab.pp
@@ -3,10 +3,14 @@ class env::big::install_g5k_jupyterlab {
     'Debian': {
       include env::common::software_versions
 
-      env::common::g5kpackages {
-        'g5k-jupyterlab':
-          ensure  => "${::env::common::software_versions::g5k_jupyterlab}",
-          release => "${::lsbdistcodename}";
+      if $::env::deb_arch != 'ppc64el' {
+        # We are stuck on building wheel for bcrypt on ppc64
+        # See https://intranet.grid5000.fr/bugzilla/show_bug.cgi?id=14311
+        env::common::g5kpackages {
+          'g5k-jupyterlab':
+            ensure  => "${::env::common::software_versions::g5k_jupyterlab}",
+            release => "${::lsbdistcodename}";
+        }
       }
     }
     default: {
