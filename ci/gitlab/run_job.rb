21732e0fdf3c3bddeaa5c772d710fb8f4261671d
Philippe Virouleau <philippe.virouleau@inria.fr>
2024-03-11 10:53:51 +0100

    Implement creating and testing images in gitlab

---
+++ ci/gitlab/run_job.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'genenvgenerator'
+require 'genenvtester'
+require 'genenvpusher'
+require 'optparse'
+require_relative 'config'
+
+options = {}
+# TODO: we could use the cloned refrepo to also:
+#  - validate the cluster
+#  - validate the site.
+
+OptionParser.new do |parser|
+  parser.on('-j JJJ', '--job', %w[build test push], 'Job to run')
+  parser.on('-e EEE', '--env', 'Environment name')
+  parser.on('-c CCC', '--cluster', 'Cluster on which to run the test (including the site, eg: "lyon-nova")')
+  parser.on('-s SSS', '--site', 'Site on which to push the environment')
+end.parse!(into: options)
+
+raise OptionParser::InvalidArgument, 'You must specify a job to run' unless options[:job]
+raise OptionParser::InvalidArgument, 'You must specify an environment name' unless options[:env]
+raise OptionParser::InvalidArgument, "Invalid environment #{options[:env]}" unless valid_env?(options[:env])
+
+case options[:job]
+when 'build'
+  GenEnvGenerator.new.generate(options[:env])
+when 'test'
+  raise OptionParser::InvalidArgument, 'You must specify a cluster' unless options[:cluster]
+
+  GenEnvTester.new.test(options[:cluster], options[:env])
+when 'push'
+  raise OptionParser::InvalidArgument, 'You must specify a site' unless options[:site]
+
+  GenEnvPusher.new.push(options[:site], options[:env])
+else
+  raise "Unknown job type: #{options[:job]}"
+end
